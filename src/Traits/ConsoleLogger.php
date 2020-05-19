d30507f284d971172cabc7b6db1b1f371f95c686
justinmoh <me@justinmoh.my>
2020-05-19 17:29:39 +0800

    added stats in console logger

--- src/Traits/ConsoleLogger.php
+++ src/Traits/ConsoleLogger.php
@@ -19,17 +19,22 @@ trait ConsoleLogger
     /** @var string $loggerChannel */
     private $loggerChannel;
 
+    /** @var float $startTime */
+    private $startTime;
+
 
     /**
      * @param  string  $message
      * @param  int  $level
+     * @param  bool  $withPeak
      */
-    protected function log($message, int $level = Logger::INFO): void
+    protected function log(string $message, int $level = Logger::INFO, bool $withPeak = false): void
     {
+        $message = implode(' ', [$this->getScriptStats($withPeak), $message]);
+
         $this->line($message, $this->getLogLevelText($level));
 
-        Log::channel($this->getLogChannel())
-            ->log($this->getLogLevelText($level, false), $message);
+        Log::channel($this->getLogChannel())->log($this->getLogLevelText($level, false), $message);
     }
 
 
@@ -79,5 +84,65 @@ trait ConsoleLogger
     private function getLogChannel()
     {
         return $this->loggerChannel;
+    }
+
+
+    /**
+     * Return stats in the format of [9,999.9MB|mm:ss].
+     *
+     * @param  bool  $withPeak
+     *
+     * @return string
+     */
+    private function getScriptStats($withPeak = false): string
+    {
+        if ($withPeak) {
+            return "[{$this->getMemoryUsage()}|{$this->getMemoryPeakUsage()}|{$this->getRunningTime()}]";
+        }
+
+        return "[{$this->getMemoryUsage()}|{$this->getRunningTime()}]";
+    }
+
+
+    /**
+     * Return script elapse time in the format of mm:ss. E.g.: 500s = '08:20' / 1000s = 16:40.
+     *
+     * @return string
+     */
+    private function getRunningTime(): string
+    {
+        if (empty($this->startTime)) {
+            $this->startTime = microtime(true);
+        }
+        $sinceStartTime = microtime(true) - $this->startTime;
+
+        return str_pad(floor($sinceStartTime / 60), 2, '0', STR_PAD_LEFT)
+            .gmdate(":s", $sinceStartTime % 60);
+    }
+
+
+    /**
+     * Return peak memory usage in the format of 9,999.9MB. E.g.: 1234.5678 = '1,234.5MB'.
+     *
+     * @return string
+     */
+    private function getMemoryPeakUsage(): string
+    {
+        $memoryPeakUsage = memory_get_peak_usage() / 1024 / 1024; // MB
+
+        return number_format($memoryPeakUsage, 1).'MB';
+    }
+
+
+    /**
+     * Return memory usage in the format of 9,999.9MB. E.g.: 1234.5678 = '1,234.5MB'.
+     *
+     * @return string
+     */
+    private function getMemoryUsage(): string
+    {
+        $memoryUsage = memory_get_usage() / 1024 / 1024; // MB
+
+        return number_format($memoryUsage, 1)."MB";
     }
 }
